ef68a32bd2ff37bd22f54e1be793c7633955af62
WSR13990 <wahyusejatiroso@yahoo.co.id>
2016-06-14 16:12:35 +0800

    The app able to fetch data from popular query, and return JSON reasult as string

--- app/src/main/java/w3sw/popularmovieapp/MainActivity.java
+++ app/src/main/java/w3sw/popularmovieapp/MainActivity.java
@@ -2,6 +2,7 @@ package w3sw.popularmovieapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment,new MainActivityFragment())
+                    .commit();
+        }
     }
 
 
--- app/src/main/java/w3sw/popularmovieapp/MainActivityFragment.java
+++ app/src/main/java/w3sw/popularmovieapp/MainActivityFragment.java
@@ -1,10 +1,24 @@
 package w3sw.popularmovieapp;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**Main activity consist of grid layout that diplay the particular movie poster
  *sorted either by popularity or by rating. The main screen can be scrolled down
@@ -25,13 +39,101 @@ import android.view.ViewGroup;
  */
 
 public class MainActivityFragment extends Fragment {
+    public static final String LOG_TAG = MainActivityFragment.class.toString();
 
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id  = item.getItemId();
+        if(id == R.id.action_refresh){
+            UpdateMovie();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void UpdateMovie(){
+        FetchMovie fetchMovie = new FetchMovie();
+        fetchMovie.execute();
+    }
+
+    public class FetchMovie extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            //These two need to be declared outside the try/catch
+            //so they can be closed in the finally block
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            //Will contain the string of the json result
+            String jsonresult = null;
+
+            //URL query parameter
+            final String POPULAR_MOVIE = "/movie/popular";
+            final String TOP_RATED_MOVIE = "/movie/top_rated";
+
+            final String API_KEY = "122c7816bb3759bd56be1d03a02d6db6";
+            final String MOVIE_BASE_URL = "https://api.themoviedb.org/3";
+
+            //Construct URL for the query search
+            Uri builtUri = Uri.parse(MOVIE_BASE_URL + POPULAR_MOVIE).buildUpon()
+                    .appendQueryParameter("api_key", API_KEY)
+                    .build();
+            try {
+                URL url = new URL(builtUri.toString());
+                //Crete the request to themoviedb and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                //Read the input stream into string
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(inputStream == null){
+                    //Nothing to do
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line +"\n");
+                }
+                if(buffer.length() ==0){
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                jsonresult = buffer.toString();
+                Log.v(LOG_TAG,jsonresult);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
